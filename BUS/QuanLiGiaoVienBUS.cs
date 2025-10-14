7c513732479d32616d8cd111c8475449063bb17f
Kahtan <0306231203@caothang.edu.vn>
2025-10-14 08:50:55 -0700

    Da thiet ke BUS

---
+++ BUS/QuanLiGiaoVienBUS.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+using Project_QuanLiSinhVien_DoAn.DAO;
+using Project_QuanLiSinhVien_DoAn.DTO;
+namespace Project_QuanLiSinhVien_DoAn.BUS
+{
+    class QuanLiGiaoVienBUS
+    {
+        QuanLiGiaoVienDAO gvDAO = new QuanLiGiaoVienDAO();
+        public DataTable DSGiaoVien()
+        {
+            return gvDAO.DSGiaoVien();
+        }
+        public bool ThemGiaoVien(GiaoVien gv, Users u)
+        {
+            return gvDAO.ThemGiaoVien(gv, u);
+        }
+        public bool SuaGiaoVien(GiaoVien gv, Users u)
+        {
+            return gvDAO.SuaGiaoVien(gv, u);
+        }
+        public bool XoaGiaoVien(GiaoVien gv, Users u)
+        {
+            return gvDAO.XoaGiaoVien(gv, u);
+        }
+        public DataTable TimGiaoVien(string hoTen)
+        {
+            return gvDAO.TimDanhSachGiaoVienTheoHoTen(hoTen);
+        }
+    }
+}
---
+++ BUS/QuanLiLopBUS.cs
@@ -0,0 +1,38 @@
+﻿using System.Data;
+using Project_QuanLiSinhVien_DoAn.DAO;
+using Project_QuanLiSinhVien_DoAn.DTO;
+namespace Project_QuanLiSinhVien_DoAn.BUS
+{
+    class QuanLiLopBUS
+    {
+        QuanLiLopDAO lopDAO = new QuanLiLopDAO();
+        public DataTable DSLop()
+        {
+            return lopDAO.DSLop();
+        }
+        public DataTable DSKhoa()
+        {
+            return lopDAO.DSKhoa();
+        }
+        public DataTable DSGiaoVien()
+        {
+            return lopDAO.DSGiaoVien();
+        }
+        public bool ThemLop(Lop l)
+        {
+            return lopDAO.ThemLop(l);
+        }
+        public bool SuaLop(Lop l)
+        {
+            return lopDAO.SuaLop(l);
+        }
+        public bool XoaLop(Lop l)
+        {
+            return lopDAO.XoaLop(l);
+        }
+        public DataTable TimLop(string tenLop)
+        {
+            return lopDAO.TimLop(tenLop);
+        }
+    }
+}
