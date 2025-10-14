22f14353fe8e82f08e096b86f580dcc9c1b0e352
khanh <0306231207@caothang.edu.vn>
2025-10-14 23:26:01 +0700

    Add DataProvider.cs thiết lập cấu hình kết nối

---
+++ DAO/DataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Project_QuanLiSinhVien_DoAn.DAO
+{
+    class DataProvider
+    {
+        public SqlConnection strConnection()
+        {
+            string query = "Data Source=DESKTOP-GB854KQ\\SQLEXPRESS;Initial Catalog=QUANLISINHVIEN_DB;Integrated Security=True";
+            SqlConnection sqlCon = new SqlConnection(query);
+            return sqlCon;
+        }
+    }
+}
