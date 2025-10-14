a275f3ee6cb66493a9aab430c1a0b6a30853296c
khanh <0306231207@caothang.edu.vn>
2025-10-14 23:56:07 +0700

    Thêm lớp DTO

---
+++ DTO/Khoa.cs
@@ -0,0 +1,13 @@
+﻿namespace Project_QuanLiSinhVien_DoAn.DTO
+{
+    class Khoa
+    {
+        public int MaKhoa { get; set; }
+        public string TenKhoa { get; set; }
+        public Khoa()
+        {
+            MaKhoa = 0;
+            TenKhoa = string.Empty;
+        }
+    }
+}
---
+++ DTO/MonHoc.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_QuanLiSinhVien_DoAn.DTO
+{
+    class MonHoc
+    {
+        public int MaMonHoc { get; set; }
+        public string TenMonHoc { get; set; }
+        public int SoTinChi { get; set; }
+        public MonHoc()
+        {
+            MaMonHoc = 0;
+            TenMonHoc = string.Empty;
+            SoTinChi = 0;
+        }
+    }
+}
