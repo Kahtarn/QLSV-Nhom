2c149605ec97fa0eb5457084119a52e849263182
Kahtan <0306231203@caothang.edu.vn>
2025-10-14 08:42:31 -0700

    Da thiet ke file DTO

---
+++ DTO/SinhVien.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project_QuanLiSinhVien_DoAn.DTO
+{
+    class SinhVien
+    {
+        public string MaSinhVien { get; set; }
+        public int MaLop { get; set; }
+        public string HoTen { get; set; }
+        public int MaUser { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string GioiTinh { get; set; }
+        public string Email { get; set; }
+        public string CCCD { get; set; }
+        public SinhVien()
+        {
+            MaSinhVien = string.Empty;
+            MaLop = 0;
+            MaUser = 0;
+            HoTen = string.Empty;
+            NgaySinh = DateTime.Now;
+            GioiTinh = string.Empty;
+            Email = string.Empty;
+            CCCD = string.Empty;
+        }
+    }
+}
---
+++ DTO/Users.cs
@@ -0,0 +1,19 @@
+﻿namespace Project_QuanLiSinhVien_DoAn.DTO
+{
+    class Users
+    {
+        public int MaUser { get; set; }
+        public string HoTen { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public string Roles { get; set; }
+        public Users()
+        {
+            MaUser = 0;
+            HoTen = string.Empty;
+            TenDangNhap = string.Empty;
+            MatKhau = string.Empty;
+            Roles = string.Empty;
+        }
+    }
+}
