694ffb3d54446807509980da14eb7fa94cfce017
ndkhanh207 <0306231207@caothang.edu.vn>
2025-10-15 13:12:13 +0700

    Them lop BUS

---
+++ project/BUS/User_SinhVienBUS.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+using Project_QuanLiSinhVien_DoAn.DAO;
+using Project_QuanLiSinhVien_DoAn.DTO;
+namespace Project_QuanLiSinhVien_DoAn.BUS
+{
+    class User_SinhVienBUS
+    {
+        User_SinhVienDAO SVDAO = new User_SinhVienDAO();
+        public DataTable loadThongTinSinhVien(Users usr)
+        {
+            return SVDAO.layThongTinSinhVien_DAO(usr);
+        }
+        public DataTable loadDiemSV(Users usr)
+        {
+            return SVDAO.layDSDiem_DAO(usr);
+        }
+        public DataTable loadTableReportSV(Users usr)
+        {
+            return SVDAO.layTable_Report_SV_DAO(usr);
+        }
+    }
+}
